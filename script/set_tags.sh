feaa6f52ae25db313037de21238c889a8b7f2998
Bison 'goldeagle' Fan <bison@bitseed.cn>
2022-09-24 22:34:25 +0800

    init with submodules

---
+++ script/set_tags.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+# buildroot
+BUILDROOT_TAG=2022.08
+cd ../buildroot
+git checkout tags/$BUILDROOT_TAG
+
+# u-boot
+UBOOT_TAG=v2022.07
+cd ../u-boot
+git checkout tags/$UBOOT_TAG
+
+# linux-kernel
+LINUX_TAG=master
+cd ../linux
+git checkout tags/$LINUX_TAG
+
+# uclibc-ng
+UCLIBC_TAG=v1.0.42
+cd ../uclibc
+git checkout tags/$UCLIBC_TAG
+
+# busybox
+BUSYBOX_TAG=1_35_0
+cd ../busybox
+git checkout tags/$BUSYBOX_TAG
+
+# openssl
+OPENSSL_TAG=openssl-3.0.5
+cd ../openssl
+git checkout tags/$OPENSSL_TAG
+
+# tools
