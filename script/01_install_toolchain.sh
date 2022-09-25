30b7051714db8a0eb7557fa008ea979d1dbe44ac
Bison 'goldeagle' Fan <bison@bitseed.cn>
2022-09-25 15:51:14 +0800

    upd: buildroot submodule & script

--- script/01_install_toolchain.sh
+++ script/01_install_toolchain.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-UTIL_PKGS="git multitail vim zsh openssh-server pixz curl"
+UTIL_PKGS="git multitail vim zsh openssh-server pixz curl tree"
 BUILD_PKGS="bison flex build-essential cmake libelf-dev gcc-arm-none-gnueabi gcc-arm-linux-gnueabi ncurses-dev musl musl-dev musl-tools"
 
 sudo apt update && sudo apt install $UTIL_PKGS BUILD_PKGS
