0a13c0336c0230af029a54c486e79d0c7333baf8
Bison 'goldeagle' Fan <bison@bitseed.cn>
2022-09-25 15:20:05 +0800

    upd: set_env.sh and submodules

--- script/02_set_tags.sh
+++ script/02_set_tags.sh
@@ -5,31 +5,31 @@ set -e
 # buildroot
 BUILDROOT_TAG=2022.08
 cd ../buildroot
-git checkout tags/$BUILDROOT_TAG
+git checkout -b nuc980 origin/nuc980
 
 # u-boot
 UBOOT_TAG=v2022.07
 cd ../u-boot
-git checkout tags/$UBOOT_TAG
+git checkout -b nuc980 origin/nuc980
 
 # linux-kernel
-LINUX_TAG=master
-cd ../linux
-git checkout tags/$LINUX_TAG
+#LINUX_TAG=master
+#cd ../linux
+#git checkout tags/$LINUX_TAG
 
 # uclibc-ng
-UCLIBC_TAG=v1.0.42
-cd ../uclibc
-git checkout tags/$UCLIBC_TAG
+#UCLIBC_TAG=v1.0.42
+#cd ../uclibc
+#git checkout tags/$UCLIBC_TAG
 
 # busybox
-BUSYBOX_TAG=1_35_0
-cd ../busybox
-git checkout tags/$BUSYBOX_TAG
+#BUSYBOX_TAG=1_35_0
+#cd ../busybox
+#git checkout tags/$BUSYBOX_TAG
 
 # openssl
-OPENSSL_TAG=openssl-3.0.5
-cd ../openssl
-git checkout tags/$OPENSSL_TAG
+#OPENSSL_TAG=openssl-3.0.5
+#cd ../openssl
+#git checkout tags/$OPENSSL_TAG
 
 # tools
