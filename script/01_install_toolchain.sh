eb2b0bf20f630eec58d1cbc6e7f98d819f5516d3
Bison 'goldeagle' Fan <bison@bitseed.cn>
2022-10-02 16:55:34 +0800

    fix: install script

--- script/01_install_toolchain.sh
+++ script/01_install_toolchain.sh
@@ -3,6 +3,6 @@
 set -e
 
 UTIL_PKGS="git multitail vim zsh openssh-server pixz curl tree"
-BUILD_PKGS="bison flex build-essential cmake libelf-dev gcc-arm-none-gnueabi gcc-arm-linux-gnueabi ncurses-dev musl musl-dev musl-tools"
+BUILD_PKGS="bison flex build-essential cmake libelf-dev gcc-arm-none-eabi gcc-arm-linux-gnueabi ncurses-dev musl musl-dev musl-tools"
 
-sudo apt update && sudo apt install $UTIL_PKGS BUILD_PKGS
+sudo apt update && sudo apt install $UTIL_PKGS $BUILD_PKGS
